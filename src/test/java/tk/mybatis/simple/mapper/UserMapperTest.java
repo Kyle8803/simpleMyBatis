4f576e26830d37085008d680812326cf540999d9
Kyle8803 <xindong395667129@126.com>
2020-07-18 22:11:17 +0800

    commit for test5

--- src/test/java/tk/mybatis/simple/mapper/UserMapperTest.java
+++ src/test/java/tk/mybatis/simple/mapper/UserMapperTest.java
@@ -2,7 +2,6 @@ package tk.mybatis.simple.mapper;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import org.apache.ibatis.session.SqlSession;
